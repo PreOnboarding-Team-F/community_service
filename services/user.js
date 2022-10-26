01bc2ce742721cd87bce80d0023766e135ce5ec9
lshyun955 <lshyun955@gmail.com>
2022-10-26 23:45:03 +0900

    Fix: 회원 가입할 때 닉네임, 전화번호, 아이디 중복 체크

--- services/user.js
+++ services/user.js
@@ -5,9 +5,20 @@ import { BadRequestException } from '../util/badRequest.exception.js';
 
 export const createUser = async userInfo => {
   const { id, password, gender, nickname, birth, phone_number } = userInfo;
-  const isExist = await userRepository.getUserById(id);
-  if (isExist) {
+  const isExistId = await userRepository.getUserById(id);
+  const isExistNickname = await userRepository.getUserByNickname(nickname);
+  const isExistPhoneNumber = await userRepository.getUserByPhoneNumber(
+    phone_number
+  );
+
+  if (isExistId) {
     throw new BadRequestException('이미 존재하는 아이디입니다.');
+  }
+  if (isExistNickname) {
+    throw new BadRequestException('이미 존재하는 닉네임입니다.');
+  }
+  if (isExistPhoneNumber) {
+    throw new BadRequestException('이미 존재하는 핸드폰 번호입니다.');
   }
 
   const salt = await bcrypt.genSalt();
