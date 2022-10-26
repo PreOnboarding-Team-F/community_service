16f4d53c7ea179856723461e72d4e1f8dc9b96f4
lshyun955 <lshyun955@gmail.com>
2022-10-26 17:25:47 +0900

    Feat: 유저 회원가입 라우터 생성 및 validtor 미들웨어 적용

---
+++ routes/user.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { body } from 'express-validator';
+import * as userController from '../controllers/user.js';
+import { validate } from '../middleware/validator.js';
+const router = express.Router();
+const validateCredeital = [
+  body('id')
+    .notEmpty()
+    .withMessage('아이디를 입력해주세요.')
+    .trim()
+    .isLength({ min: 7, max: 15 })
+    .withMessage('아이디를 올바른 양식으로 작성해주세요.'),
+  body('password')
+    .notEmpty()
+    .withMessage('비밀번호를 입력해주세요.')
+    .trim()
+    .matches(
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{10,20}$/
+    )
+    .withMessage('비밀번호를 올바른 형식으로 입력해주세요.'),
+  body('nickname')
+    .notEmpty()
+    .withMessage('닉네임을 입력해주세요.')
+    .trim()
+    .matches(/^([ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]).{7,15}$/)
+    .withMessage('닉네임을 올바른 형식으로 입력해주세요.'),
+  validate,
+];
+// 회원 가입
+router.post('/register', validateCredeital, userController.createUser);
