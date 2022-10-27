5de3f47aacb72b9919801c524a68156a7d511c71
박주현 <leehot2665@naver.com>
2022-10-27 15:21:07 +0900

    Refactor: prettier 설정 안된 코드 수정

--- models/statistics.js
+++ models/statistics.js
@@ -1,17 +1,17 @@
 import prismaClient from './prismaClient.js';
 
 export const readAllUserGender = async () => {
-    const data =  prismaClient.user.groupBy({
-        by:['gender'],
-        _count:{
-            gender:true
-        }
-    })
-    return data;
+  const data = prismaClient.user.groupBy({
+    by: ['gender'],
+    _count: {
+      gender: true,
+    },
+  });
+  return data;
 };
 
 export const readAllUserGenderByBoard = async () => {
-    const data = prismaClient.$queryRaw`
+  const data = prismaClient.$queryRaw`
     SELECT COUNT(*) AS genderCount, user.gender
     FROM board JOIN user ON board.user_id = user.id
     GROUP BY  user.gender`;
@@ -20,14 +20,14 @@ export const readAllUserGenderByBoard = async () => {
 };
 
 export const readAllUserAge = async () => {
-    const data = prismaClient.$queryRaw`
+  const data = prismaClient.$queryRaw`
     SELECT YEAR(NOW())-LEFT(user.birth,4) +1 AS age
     FROM user`;
-    return data;
+  return data;
 };
 
 export const readAllUserAgeByBoard = async () => {
-    const data = prismaClient.$queryRaw`
+  const data = prismaClient.$queryRaw`
     SELECT YEAR(NOW())-LEFT(user.birth,4) +1 AS age
     FROM board JOIN user ON board.user_id = user.id`;
   return data;
